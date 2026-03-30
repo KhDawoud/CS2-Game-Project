4e11cfffdf49f4cc7624cf4decc0eddb47fdba66
Khaled Dawoud <kdawoud56@gmail.com>
2026-03-30 03:35:32 +0200

    Created player sprite with state and animation manager, walk and idle animation implemented

--- player.cpp
+++ player.cpp
@@ -1,60 +1,171 @@
 #include "player.hpp"
-#include <QBrush>
 #include <QKeyEvent>
 
 Player::Player()
 {
-    setRect(0, 0, 50, 50);
-    setBrush(Qt::red);
+    currentState = PlayerState::Idle;
+    currentDirection = Direction::Right;
+    currentFrame = 0;
+
+    walkSheet.load("resources/player/running/swordsman_1_run.png");
+    idleSheet.load("resources/player/idling/swordsman_1_idle.png");
+    walkFrameWidth = walkSheet.width() / 8;
+    walkFrameHeight = walkSheet.height() / 4;
+
+    idleFrameWidth = idleSheet.width() / 12;
+    idleFrameHeight = idleSheet.height() / 4;
+
+    setScale(2.0);
+
+    animTimer = new QTimer(this);
+    connect(animTimer, &QTimer::timeout, this, &Player::updateAnimation);
+
+    idleTimer = new QTimer(this);
+    idleTimer->setSingleShot(true);
+    connect(idleTimer, &QTimer::timeout, this, [this]()
+            { setAnimationState(PlayerState::Idle); });
+
+    setAnimationState(PlayerState::Idle);
+
+    setPixmap(idleSheet.copy(0, static_cast<int>(currentDirection) * idleFrameHeight, idleFrameWidth, idleFrameHeight));
+
+    QTimer *moveTimer = new QTimer(this);
+    connect(moveTimer, &QTimer::timeout, this, &Player::movePlayer);
+    moveTimer->start(16);
 
     setFlag(QGraphicsItem::ItemIsFocusable);
     setFocus();
 }
 
+void Player::setAnimationState(PlayerState newState)
+{
+    if (currentState == newState)
+        return;
+
+    currentState = newState;
+    currentFrame = 0;
+
+    if (currentState == PlayerState::Idle)
+    {
+        animTimer->start(200); // Was cycling too fast so I made it slower
+    }
+    else
+    {
+        animTimer->start(100);
+    }
+}
+
+void Player::updateAnimation()
+{
+
+    bool isWaitingToIdle = (currentState == PlayerState::Walking && activeKeys.isEmpty());
+
+    int currentFrameWidth = 0;
+    int currentFrameHeight = 0;
+    QPixmap *sheetToDraw = nullptr; // A pointer to easily swap which image we use
+
+    if (currentState == PlayerState::Idle || isWaitingToIdle)
+    {
+        maxFrames = (currentDirection == Direction::Up) ? 4 : 12;
+        currentFrameWidth = idleFrameWidth;
+        currentFrameHeight = idleFrameHeight;
+        sheetToDraw = &idleSheet;
+    }
+    else
+    {
+        maxFrames = 8;
+        currentFrameWidth = walkFrameWidth;
+        currentFrameHeight = walkFrameHeight;
+        sheetToDraw = &walkSheet;
+    }
+
+    if (isWaitingToIdle)
+    {
+        currentFrame = 0;
+    }
+    else
+    {
+        // so switching directions correctly updates the max frames
+        if (currentFrame >= maxFrames)
+        {
+            currentFrame = 0;
+        }
+        currentFrame = (currentFrame + 1) % maxFrames;
+    }
+
+    int row = static_cast<int>(currentDirection);
+    setPixmap(sheetToDraw->copy(currentFrame * currentFrameWidth, row * currentFrameHeight, currentFrameWidth, currentFrameHeight));
+}
+
+void Player::movePlayer()
+{
+    if (activeKeys.isEmpty())
+    {
+        if (currentState == PlayerState::Walking && !idleTimer->isActive())
+        {
+            idleTimer->start(1000); // we only idle after 0.5 seconds of doing nothing
+        }
+        return;
+    }
+
+    idleTimer->stop();
+    setAnimationState(PlayerState::Walking);
+
+    // so keys dont overlap whatever is pressed is added to a list and only
+    // most recent input is considered
+
+    Qt::Key newestKey = activeKeys.last();
+    int speed = 4;
+
+    switch (newestKey)
+    {
+    case Qt::Key_Right:
+    case Qt::Key_D:
+        currentDirection = Direction::Right;
+        setPos(x() + speed, y());
+        break;
+    case Qt::Key_Left:
+    case Qt::Key_A:
+        currentDirection = Direction::Left;
+        setPos(x() - speed, y());
+        break;
+    case Qt::Key_Up:
+    case Qt::Key_W:
+        currentDirection = Direction::Up;
+        setPos(x(), y() - speed);
+        break;
+    case Qt::Key_Down:
+    case Qt::Key_S:
+        currentDirection = Direction::Down;
+        setPos(x(), y() + speed);
+        break;
+    default:
+        break;
+    }
+}
+
 void Player::keyPressEvent(QKeyEvent *event)
 {
-    if (event->key() == Qt::Key_Left)
+    // so we don't double count holding down the button
+    if (event->isAutoRepeat())
+        return;
+    Qt::Key key = static_cast<Qt::Key>(event->key());
+
+    // for now only adding movement but obv more controls will be added
+    if (key == Qt::Key_W || key == Qt::Key_A || key == Qt::Key_S || key == Qt::Key_D ||
+        key == Qt::Key_Up || key == Qt::Key_Down || key == Qt::Key_Left || key == Qt::Key_Right)
     {
-        if (event->modifiers() & Qt::ShiftModifier)
+        if (!activeKeys.contains(key))
         {
-            moveBy(-20, 0);
-        }
-        else
-        {
-            moveBy(-10, 0);
+            activeKeys.append(key);
         }
     }
-    else if (event->key() == Qt::Key_Right)
-    {
-        if (event->modifiers() & Qt::ShiftModifier)
-        {
-            moveBy(20, 0);
-        }
-        else
-        {
-            moveBy(10, 0);
-        }
-    }
-    else if (event->key() == Qt::Key_Up)
-    {
-        if (event->modifiers() & Qt::ShiftModifier)
-        {
-            moveBy(0, -20);
-        }
-        else
-        {
-            moveBy(0, -10);
-        }
-    }
-    else if (event->key() == Qt::Key_Down)
-    {
-        if (event->modifiers() & Qt::ShiftModifier)
-        {
-            moveBy(0, 20);
-        }
-        else
-        {
-            moveBy(0, 10);
-        }
-    }
+}
+
+void Player::keyReleaseEvent(QKeyEvent *event)
+{
+    if (event->isAutoRepeat())
+        return;
+    Qt::Key key = static_cast<Qt::Key>(event->key());
+    activeKeys.removeAll(key);
 }
--- player.hpp
+++ player.hpp
@@ -1,9 +1,55 @@
-#include <QGraphicsRectItem>
-#include <QKeyEvent>
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QTimer>
 
-class Player : public QGraphicsRectItem{
+enum class PlayerState
+{
+    Idle,
+    Walking
+};
+
+enum class Direction
+{
+    Right = 2,
+    Up = 3,
+    Left = 1,
+    Down = 0
+};
+
+class Player : public QObject, public QGraphicsPixmapItem
+{
+    Q_OBJECT
 public:
     Player();
+
+public slots:
+    void updateAnimation();
+    void movePlayer();
+
 protected:
     void keyPressEvent(QKeyEvent *event) override;
+    void keyReleaseEvent(QKeyEvent *event) override;
+
+private:
+    QTimer *animTimer;
+    QTimer *idleTimer;
+
+    PlayerState currentState;
+    Direction currentDirection;
+    int currentFrame;
+
+    QPixmap walkSheet;
+    QPixmap idleSheet;
+
+    int walkFrameWidth;
+    int walkFrameHeight;
+
+    int idleFrameWidth;
+    int idleFrameHeight;
+
+    int maxFrames;
+
+    QList<Qt::Key> activeKeys;
+
+    void setAnimationState(PlayerState newState);
 };
