9d52aa19cd368507fcdf47c7df54d49de9da6768
mohamedgreida <mohamed_greida@aucegypt.edu>
2026-04-03 23:42:33 +0200

    Changed view

--- main.cpp
+++ main.cpp
@@ -2,23 +2,31 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include "player.hpp"
+#include "map.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QGraphicsScene *scene = new QGraphicsScene();
-    // the reason for the weird slowdown at the start was this wasn't set.
-    // when u start, it was unaware of the size of the scene so just drew a box around you and once
-    // u pass it it extends the box, causing the slowdown.
-    scene->setSceneRect(0, 0, 800, 600);
-    Player *player = new Player();
+    Map *scene = new Map();
+    scene->setSceneRect(0, 0, 40*32 ,35*32);
 
+
+    Player *player = new Player();
+    player->setPos(40*7,35*28);
     scene->addItem(player);
 
     QGraphicsView *view = new QGraphicsView(scene);
-    view->setFixedSize(800, 600);
-    view->show();
+    view->setAlignment(Qt::AlignCenter);
 
+    view->scale(2.0, 2.0);
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    view->centerOn(player);
+    QObject::connect(player, &Player::positionChanged, view, [view](QGraphicsItem* p){
+        view->centerOn(p);
+    });
+    view->showMaximized();
     return a.exec();
 }
