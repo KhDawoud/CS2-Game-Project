57c10eee6568af6d24ebdc37238d3dbdc017104b
Malak0098 <malakhosny101@gmail.com>
2026-04-04 15:29:27 +0200

    Fix syntax error in loadAnimations function

--- slime.cpp
+++ slime.cpp
@@ -7,7 +7,7 @@ Slime::Slime() : BaseEnemy(50, 5, 2, 1.5f,40.0) // hp, atk, def, speed
 void Slime::loadAnimations()
 {
     idleSheet.load(":resources/enemy/idle/Slime1_Idle_with_shadow.png");
-    walkSheet.load(":resources/enemy/walk/Slime1_Walk_with_shadow.png"));
+    walkSheet.load(":resources/enemy/walk/Slime1_Walk_with_shadow.png");
     attackSheet.load(":resources/enemy/attack/Slime1_Attack_with_shadow.png");
 
         idleData.frameCount = 6;
