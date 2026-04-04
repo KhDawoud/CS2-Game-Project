1b9ec87ef810e06c54c1c4074c8d6b0b130318f6
Khaled Dawoud <kdawoud56@gmail.com>
2026-04-04 02:51:55 +0200

    Changed zoom and player speed. Made sound effect system. Refactored map code so that the map vector is read from a file and the tile images are in a hashtable so it's a bit more readable.

---
+++ AudioManager.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <QObject>
+#include <QSoundEffect>
+#include <QUrl>
+#include <unordered_map>
+#include <string>
+#include <QDebug>
+
+// This is basically a class which is instantiated once and never again and everyone can access
+// it through AudioManager::instance() so all sound effects are handled in one place
+class AudioManager : public QObject
+{
+public:
+    static AudioManager &instance()
+    {
+        static AudioManager _instance;
+        return _instance;
+    }
+
+    void playSound(const std::string &key)
+    {
+        auto it = soundTable.find(key);
+        if (it != soundTable.end())
+        {
+            it->second->play();
+        }
+        else
+        {
+            qWarning() << "Sound key not found:" << QString::fromStdString(key);
+        }
+    }
+
+    AudioManager(const AudioManager &) = delete;
+    AudioManager &operator=(const AudioManager &) = delete;
+
+private:
+    AudioManager(QObject *parent = nullptr) : QObject(parent)
+    {
+        QSoundEffect *grassWalk = new QSoundEffect(this);
+        grassWalk->setSource(QUrl("qrc:/resources/sound-effects/GrassWalk.wav"));
+        grassWalk->setVolume(0.3f);
+        soundTable["GrassWalk"] = grassWalk;
+
+        QSoundEffect *swordSwing = new QSoundEffect(this);
+        swordSwing->setSource(QUrl("qrc:/resources/sound-effects/SwordSwing.wav"));
+        swordSwing->setVolume(0.5f);
+        soundTable["SwordSwing"] = swordSwing;
+
+
+    }
+
+    std::unordered_map<std::string, QSoundEffect *> soundTable;
+};
--- main.cpp
+++ main.cpp
@@ -2,31 +2,32 @@
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include "player.hpp"
-#include "map.h"
+#include "map.hpp"
+#include "AudioManager.hpp"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    Map *scene = new Map();
-    scene->setSceneRect(0, 0, 40*32 ,35*32);
+    AudioManager::instance();
 
+    Map *scene = new Map();
+    scene->setSceneRect(0, 0, 40 * 32, 35 * 32);
 
     Player *player = new Player();
-    player->setPos(40*7,35*28);
+    player->setPos(40 * 7, 35 * 28);
     scene->addItem(player);
 
     QGraphicsView *view = new QGraphicsView(scene);
     view->setAlignment(Qt::AlignCenter);
 
-    view->scale(2.0, 2.0);
+    view->scale(3.0, 3.0);
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     view->centerOn(player);
-    QObject::connect(player, &Player::positionChanged, view, [view](QGraphicsItem* p){
-        view->centerOn(p);
-    });
+    QObject::connect(player, &Player::positionChanged, view, [view](QGraphicsItem *p)
+                     { view->centerOn(p); });
     view->showMaximized();
     return a.exec();
 }
