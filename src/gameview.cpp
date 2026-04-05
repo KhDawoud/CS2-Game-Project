f3ce7641614af9d311a0085a7e8720d4cea84165
mohamedgreida <mohamed_greida@aucegypt.edu>
2026-04-05 20:11:28 +0200

    Changed font and dimmed window when paused

--- src/gameview.cpp
+++ src/gameview.cpp
@@ -1,7 +1,10 @@
 #include "gameview.hpp"
 #include <QKeyEvent>
+#include <QGraphicsColorizeEffect>
 #include "pausewindow.hpp"
-GameView::GameView(Map* scene,Player* player): QGraphicsView(scene) {
+GameView::GameView(Map *scene, Player *player)
+    : QGraphicsView(scene)
+{
     setAlignment(Qt::AlignCenter);
 
     scale(3.0, 3.0);
@@ -11,13 +14,20 @@ GameView::GameView(Map* scene,Player* player): QGraphicsView(scene) {
     showFullScreen();
 }
 
-void GameView::keyPressEvent(QKeyEvent *event) {
+void GameView::keyPressEvent(QKeyEvent *event)
+{
     if (event->key() == Qt::Key_Escape) {
+        QGraphicsColorizeEffect *dim = new QGraphicsColorizeEffect();
+        dim->setColor(Qt::black);
+        dim->setStrength(0.7); // 70% dark
+        this->setGraphicsEffect(dim);
+
         // Create the popup
         pausewindow window(this);
 
         // This stops the game (blocks) until the user clicks a button
         window.exec();
+        this->setGraphicsEffect(nullptr);
     } else {
         // Pass other keys (movement) to the base class
         QGraphicsView::keyPressEvent(event);
