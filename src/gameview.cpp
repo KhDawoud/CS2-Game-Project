e712f36a95f7351ea940ff4e7a4fe7e69d68b392
mohamedgreida <mohamed_greida@aucegypt.edu>
2026-05-08 00:31:09 +0300

    fiexed player swtiching

--- src/gameview.cpp
+++ src/gameview.cpp
@@ -102,25 +102,11 @@ void GameView::keyPressEvent(QKeyEvent *event)
             switchToOverworld();
         }
     }
-    else if (event->key() >= Qt::Key_1 && event->key() <= Qt::Key_3)
+    else if (event->key() >= Qt::Key_1 && event->key() <= Qt::Key_4)
     {
-        if(_player->getcharacternum() == 1) return;
-        _player->swtichto(1);
-        _player->setScale(1.2f);
-
-    }else if (event->key() == Qt::Key_2)
-    {
-        if(_player->getcharacternum() == 2) return;
-        _player->swtichto(2);
-        _player->setScale(1.2f);
-    }else if (event->key() == Qt::Key_3)
-    {
-        if(_player->getcharacternum() == 3) return;
-        _player->swtichto(3);
-        _player->setScale(1.2f);
-    }else if (event->key() == Qt::Key_4){
-        if(_player->getcharacternum() == 4) return;
-        _player->swtichto(4);
+        int targetChar = event->key() - Qt::Key_1 + 1;
+        if(_player->getcharacternum() == targetChar) return;
+        _player->swtichto(targetChar);
         _player->setScale(1.2f);
     }
     else
